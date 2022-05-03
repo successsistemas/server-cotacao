df662411ff31138fd3fa8484a5c239a4e66db5cb
Lucas Silva <lucassilvaee1245@gmail.com>
2022-05-03 12:32:30 -0300

    prefizUrl

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    allowedHeaders: [
-      'allowedHeaders'
-    ],
-    origin: [
-      'http://localhost:3005/',
-      'http://localhost:3001',
-      'http://localhost:5000',
-      //   'http://localhost:3001',
-    ],
-    methods: ["GET", "POST"],
+    allowedHeaders: "*",
+    origin: "*",
   })
 
   //app.enableCors();
