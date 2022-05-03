a6c732ead682527608e48613dd819cbcd49a15af
Lucas Silva <lucassilvaee1245@gmail.com>
2022-05-03 13:16:19 -0300

    prefizUrl

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  //app.enableCors({ credentials: true, origin: "http://localhost:3005" })
+  app.enableCors({ credentials: true, origin: "http://localhost:3005" })
 
   // app.enableCors({
   //   origin: [
@@ -34,7 +34,7 @@ async function bootstrap() {
   //   ],
   // })
 
-  app.enableCors();
+  // app.enableCors();
   //await app.listen(4005);
   await app.listen(3050);
 }
